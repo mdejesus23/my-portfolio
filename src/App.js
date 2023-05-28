eb50f6ea4189ce1c992c07f0b7cf0e77f5dd4a46
Melnard <dejesusmelnard@gmail.com>
2023-05-28 23:49:53 +0800

    fixed routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProjectsPage from "./pages/Project";
 import ContactPage from "./pages/Contact";
 import ErrorPage from "./pages/Error";
 
-const router = createBrowserRouter([
+const routes = [
   {
     path: "/",
     element: <RootLayout />,
@@ -15,41 +15,76 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "Skills", element: <SkillsPage /> },
-      {
-        path: "Projects",
-        element: <ProjectsPage />,
-        // loader: async () => {
-        //   const response = await fetch(
-        //     "https://react-custom-http-8477e-default-rtdb.firebaseio.com/Projects.json"
-        //   );
-
-        //   if (!response.ok) {
-        //     // throw new Error("Something went wrong");
-        //   } else {
-        //     const resData = await response.json();
-
-        //     const projects = [];
-
-        //     for (const key in resData) {
-        //       projects.push({
-        //         id: key,
-        //         name: resData[key].name,
-        //         image: resData[key.image],
-        //         link: resData[key].link,
-        //       });
-        //     }
-
-        //     return projects; // this data will be use to the ProjectsPage component and child compoenent
-        //   }
-        // },
-      },
+      { path: "Projects", element: <ProjectsPage /> },
       { path: "Contact", element: <ContactPage /> },
     ],
   },
-]);
+];
+
+const router = createBrowserRouter({
+  routes,
+  basename: process.env.PUBLIC_URL,
+});
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import RootLayout from "./pages/Root";
+// import HomePage from "./pages/Home";
+// import SkillsPage from "./pages/Skills";
+// import ProjectsPage from "./pages/Project";
+// import ContactPage from "./pages/Contact";
+// import ErrorPage from "./pages/Error";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       { index: true, element: <HomePage /> },
+//       { path: "Skills", element: <SkillsPage /> },
+//       {
+//         path: "Projects",
+//         element: <ProjectsPage />,
+//       },
+//       { path: "Contact", element: <ContactPage /> },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
+// loader: async () => {
+//   const response = await fetch(
+//     "https://react-custom-http-8477e-default-rtdb.firebaseio.com/Projects.json"
+//   );
+
+//   if (!response.ok) {
+//     // throw new Error("Something went wrong");
+//   } else {
+//     const resData = await response.json();
+
+//     const projects = [];
+
+//     for (const key in resData) {
+//       projects.push({
+//         id: key,
+//         name: resData[key].name,
+//         image: resData[key.image],
+//         link: resData[key].link,
+//       });
+//     }
+
+//     return projects; // this data will be use to the ProjectsPage component and child compoenent
+//   }
+// },
